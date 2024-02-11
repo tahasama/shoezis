4c349b6d7c0e3a5725eb9be9a4a678ba9675ebbb
tahasama <taha.maatof@gmail.com>
2024-02-11 23:50:52 +0100

    added font to home tabs

--- HomePage/LowerStack/Home/Home.tsx
+++ HomePage/LowerStack/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { useFonts } from "expo-font";
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Kids from "./HomeTabs/Kids";
@@ -8,8 +8,22 @@ import Women from "./HomeTabs/Women";
 const HomeTab = createMaterialTopTabNavigator();
 
 const Home = () => {
+  const [fontsLoaded] = useFonts({
+    "HelveticaNeue-Regular": require("../../../assets/fonts/HelveticaNeue-Regular.otf"),
+    "HelveticaNeue-Bold": require("../../../assets/fonts/HelveticaNeue-Bold.otf"),
+  });
   return (
-    <HomeTab.Navigator>
+    <HomeTab.Navigator
+      screenOptions={(route) => ({
+        tabBarItemStyle: {
+          width: "auto",
+          marginHorizontal: 10,
+        },
+        tabBarLabelStyle: {
+          fontFamily: "HelveticaNeue-Bold",
+        },
+      })}
+    >
       <HomeTab.Screen name="Women" component={Women} />
       <HomeTab.Screen name="Men" component={Men} />
       <HomeTab.Screen name="Kids" component={Kids} />
